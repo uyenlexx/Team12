bd1bfe597fede099cf65b0aa206779281d29193e
uyenlex <21020672@vnu.edu.vn>
2023-10-12 11:42:07 +0700

    home screen (unfinished)

---
+++ app/src/main/java/com/example/team12/components/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.team12.components;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.team12.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
