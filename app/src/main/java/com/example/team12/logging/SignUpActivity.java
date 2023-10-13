faffc8e6373db0c1c9311fa8c3092f1bf8269734
HieuNguyen <hieu1282003@gmail.com>
2023-10-13 21:40:58 +0700

    Register v1

--- app/src/main/java/com/example/team12/logging/SignUpActivity.java
+++ app/src/main/java/com/example/team12/logging/SignUpActivity.java
@@ -1,17 +1,64 @@
 package com.example.team12.logging;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.team12.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
-    Button signUpButton;
+    AppCompatButton signUpButton;
+    EditText usernameInput;
+    EditText passwordInput;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUpButton = (AppCompatButton) findViewById(R.id.signup_button);
+        usernameInput = (EditText) findViewById(R.id.username_input);
+        passwordInput = (EditText) findViewById(R.id.password_input);
+        mAuth = FirebaseAuth.getInstance();
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+
+                                    Toast.makeText(SignUpActivity.this, "Authentication success.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/team12/onboarding/SplashScreenActivity.java
+++ app/src/main/java/com/example/team12/onboarding/SplashScreenActivity.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.example.team12.MainActivity;
 import com.example.team12.R;
+import com.example.team12.logging.LogInActivity;
+import com.example.team12.logging.SignUpActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
     Handler handler;
@@ -19,9 +22,9 @@ public class SplashScreenActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, NavigationActivity.class));
+                startActivity(new Intent(SplashScreenActivity.this, LogInActivity.class));
                 finish();
             }
-        }, 3000); // 3 seconds
+        }, 1000); // 3 seconds
     }
 }
