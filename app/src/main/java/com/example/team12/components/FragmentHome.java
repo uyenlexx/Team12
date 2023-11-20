5dec5478a3f0ca2fb3b0f1cd66a24edd926e2ecf
uyenlex <21020672@vnu.edu.vn>
2023-11-21 01:12:11 +0700

    bmi, search, profile screen

--- app/src/main/java/com/example/team12/components/FragmentHome.java
+++ app/src/main/java/com/example/team12/components/FragmentHome.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -17,12 +18,34 @@ public class FragmentHome extends Fragment {
     TextView helloTitleDesc;
     RelativeLayout caloriesCalculatorContainer;
     RelativeLayout menuPlannerContainer;
+    Button caloriesCalculatorButton;
+    Button menuPlannerButton;
 
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View homeView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        helloTitle = homeView.findViewById(R.id.hello_title);
+        helloTitleDesc = homeView.findViewById(R.id.hello_desc_title);
+        caloriesCalculatorContainer = homeView.findViewById(R.id.calories_cal_container);
+        menuPlannerContainer = homeView.findViewById(R.id.menu_builder_container);
+        caloriesCalculatorButton = homeView.findViewById(R.id.calories_cal_btn);
+        menuPlannerButton = homeView.findViewById(R.id.menu_builder_btn);
+
+        caloriesCalculatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        menuPlannerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return homeView;
-
-
     }
 }
