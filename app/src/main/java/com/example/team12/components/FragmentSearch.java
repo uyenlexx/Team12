4cb1fe923b1d2115748693ee286e2506d42f73f5
uyenlex <21020672@vnu.edu.vn>
2023-11-23 00:49:21 +0700

    home, search screen updated

--- app/src/main/java/com/example/team12/components/FragmentSearch.java
+++ app/src/main/java/com/example/team12/components/FragmentSearch.java
@@ -2,19 +2,75 @@ package com.example.team12.components;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.team12.R;
+import com.example.team12.components.search.ChildModelClass;
+import com.example.team12.components.search.ParentAdapter;
+import com.example.team12.components.search.ParentModelClass;
+
+import java.util.ArrayList;
+
 public class FragmentSearch extends Fragment {
+    SearchView searchBar;
+    RecyclerView recyclerView;
+    ArrayList<ParentModelClass> parentModelClasses;
+    ArrayList<ChildModelClass> childModelClasses;
+    ArrayList<ChildModelClass> ingredientsArrayList;
+    ArrayList<ChildModelClass> recipeArrayList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        View searchView = inflater.inflate(R.layout.fragment_search, container, false);
+        return searchView;
+    }
 
-        return inflater.inflate(R.layout.fragment_search, container, false);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        searchBar = view.findViewById(R.id.search_input);
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        recyclerView = view.findViewById(R.id.search_recycler_view);
+        childModelClasses = new ArrayList<>();
+        ingredientsArrayList = new ArrayList<>();
+        recipeArrayList = new ArrayList<>();
+        parentModelClasses = new ArrayList<>();
+
+        ingredientsArrayList.add(new ChildModelClass(R.drawable.img_fruits, getText(R.string.fruits).toString(), R.color.fade_red));
+        ingredientsArrayList.add(new ChildModelClass(R.drawable.img_vegetables, getText(R.string.vegetables).toString(), R.color.fade_green));
+        ingredientsArrayList.add(new ChildModelClass(R.drawable.img_meat, getText(R.string.meat).toString(), R.color.fade_yellow));
+        ingredientsArrayList.add(new ChildModelClass(R.drawable.img_seafood, getText(R.string.seafood).toString(), R.color.fade_blue));
+
+        parentModelClasses.add(new ParentModelClass(getText(R.string.ingredients_category).toString(), ingredientsArrayList));
+
+        recipeArrayList.add(new ChildModelClass(R.drawable.img_dessert, getText(R.string.dessert).toString(), R.color.fade_red));
+        recipeArrayList.add(new ChildModelClass(R.drawable.img_drinks, getText(R.string.drink).toString(), R.color.fade_green));
+        recipeArrayList.add(new ChildModelClass(R.drawable.img_fried, getText(R.string.fried).toString(), R.color.fade_yellow));
+        recipeArrayList.add(new ChildModelClass(R.drawable.img_grilled, getText(R.string.grilled).toString(), R.color.fade_blue));
+        recipeArrayList.add(new ChildModelClass(R.drawable.img_steamed, getText(R.string.steamed).toString(), R.color.fade_purple));
+
+        parentModelClasses.add(new ParentModelClass(getText(R.string.recipes_category).toString(), recipeArrayList));
+
+        ParentAdapter parentAdapter = new ParentAdapter(parentModelClasses, getContext());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerView.setAdapter(parentAdapter);
+        parentAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/com/example/team12/components/search/ParentAdapter.java
@@ -0,0 +1,58 @@
+package com.example.team12.components.search;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.team12.R;
+
+import java.util.List;
+
+public class ParentAdapter extends RecyclerView.Adapter<ParentAdapter.ViewHolder> {
+    List<ParentModelClass> parentModelClassList;
+    Context context;
+
+    public ParentAdapter(List<ParentModelClass> parentModelClassList, Context context) {
+        this.parentModelClassList = parentModelClassList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ParentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.fragment_search_parent_rv, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ParentAdapter.ViewHolder holder, int position) {
+        holder.category_title.setText(parentModelClassList.get(position).category_title);
+        ChildAdapter childAdapter =
+                new ChildAdapter(parentModelClassList.get(position).childModelClassList, context);
+        holder.rv_child.setLayoutManager(new LinearLayoutManager(context, RecyclerView.HORIZONTAL, false));
+        holder.rv_child.setAdapter(childAdapter);
+        childAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return parentModelClassList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        RecyclerView rv_child;
+        TextView category_title;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            rv_child = itemView.findViewById(R.id.category_rv_child);
+            category_title = itemView.findViewById(R.id.category_text);
+        }
+    }
+}
