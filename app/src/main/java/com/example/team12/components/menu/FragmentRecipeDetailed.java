b28720fd700b3348007edf46070402699d2c1c60
HieuNguyen <hieu1282003@gmail.com>
2023-11-30 00:48:00 +0700

    Testing on Recipe Details

--- app/src/main/java/com/example/team12/components/menu/FragmentRecipeDetailed.java
+++ app/src/main/java/com/example/team12/components/menu/FragmentRecipeDetailed.java
@@ -28,8 +28,24 @@ public class FragmentRecipeDetailed extends Fragment {
         this.backFragment = backFragment;
     }
 
+
+
 //    TextView step;
     WebView webView;
+    TextView title, calories, protein, carbs, fat, details;
+
+    public void showData(String title, Double calories,
+                         Double protein, Double carbs,
+                         Double fat, String details, String url
+                        ) {
+        this.title.setText(title);
+        this.calories.setText(calories.toString());
+        this.protein.setText(protein.toString());
+        this.carbs.setText(carbs.toString());
+        this.fat.setText(fat.toString());
+        this.details.setText(details);
+        webView.loadUrl(url);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -39,7 +55,20 @@ public class FragmentRecipeDetailed extends Fragment {
 //        step.setText(Html.fromHtml());
         String url = "https://firebasestorage.googleapis.com/v0/b/calo-a7a97.appspot.com/o/recipefriedegg.html?alt=media&token=16b42617-ff71-4af8-b115-38b083aa3ece";
         webView = recipeDetailedView.findViewById(R.id.recipe_steps);
-        webView.loadUrl(url);
+        title = recipeDetailedView.findViewById(R.id.recipe_title);
+        calories = recipeDetailedView.findViewById(R.id.recipe_calories_number);
+        protein = recipeDetailedView.findViewById(R.id.recipe_protein_number);
+        carbs = recipeDetailedView.findViewById(R.id.recipe_carbs_number);
+        fat = recipeDetailedView.findViewById(R.id.recipe_fat_number);
+        details = recipeDetailedView.findViewById(R.id.recipe_details);
+
+
+        showData("Fried Egg", 100.0, 10.0, 10.0, 10.0, "Fried egg is a dish made of egg, " +
+                "fried in oil or butter. Fried eggs are traditionally eaten for breakfast in " +
+                "many countries but may also be served at other times of the day. There is a " +
+                "wide variety of ways to cook fried eggs. Many cuisines have their own " +
+                "versions of fried eggs, such as the sunny side up, the Denver omelette, " +
+                "the Scotch egg, the Thai fried egg, and others.", url);
         return recipeDetailedView;
     }
 
